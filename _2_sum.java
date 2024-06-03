a5055a290bf777b270810aaaed10eb476090e4c1
Alok955 <deygobindagagan@gmail.com>
2024-06-04 01:41:59 +0530

    sum of two number

---
+++ _2_sum.java
@@ -0,0 +1,10 @@
+public class _2_sum {
+  public static void main(String[] args) {
+    int a=10;
+    int b=30;
+    int c=a+b;
+    System.out.println("a:"+a);
+    System.out.println("b:"+b);
+    System.out.println("sum:"+ c);
+  }
+}
