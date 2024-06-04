72f324dac6ffdf6e9335c441450c686bef2f6460
GOBINDA GAGAN DEY <130830209+Developer-Alok@users.noreply.github.com>
2024-06-04 08:38:00 +0530

    Update _4_largest_Of_Three__Number.java

--- _4_largest_Of_Three_Number.java
+++ _4_largest_Of_Three_Number.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
-
 public class _4_largest_Of_Three_Number {
   public static void main(String args[]) {
     while (true) {
-
       Scanner input = new Scanner(System.in);
       System.out.println("Enter three number");
       System.out.print("num1:");
@@ -12,19 +10,14 @@ public class _4_largest_Of_Three_Number {
       int num2 = input.nextInt();
       System.out.print("num3:");
       int num3 = input.nextInt();
-
       System.out.println("num1:" + num1 + "  " + "num2:" + num2 + "  " + "num3:" + num3);
-
       if (num1 > num2 && num1 > num3) {
         System.out.println("Largest Number:" + num1);
       } else if (num2 > num1 && num2 > num3) {
         System.out.println("Largest Number:" + num2);
-
       } else {
         System.out.println("Largest Number:" + num3);
-
       }
-
       System.out.println();
     }
   }
