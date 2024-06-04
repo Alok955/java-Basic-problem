ae4ada27db14ab0bbf36f72f42f7d9096938f151
Alok955 <deygobindagagan@gmail.com>
2024-06-04 22:27:48 +0530

    Factorial of a Number

--- _5_FactorialNumber.java
+++ _5_FactorialNumber.java
@@ -4,6 +4,7 @@ public class _5_FactorialNumber {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.print("enter a number:");
+    
     int num = sc.nextInt();
     int fact = 1;
     if (num < 0) {
